8b933e6c2cd938b9d48296ebe4605da5af530228
russellcoleman <russell@russelltcoleman.com>
2023-03-19 23:18:54 -0400

    update admin site

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -26,6 +26,11 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice']
 
 # <HINT> Register Question and Choice models here
 
@@ -33,3 +38,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
